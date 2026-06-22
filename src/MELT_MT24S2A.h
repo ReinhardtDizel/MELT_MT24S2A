a061b93d32d135ecb660eebd6866bbff43fee562
Michael <dizel882@gmail.com>
2026-06-22 09:21:21 +0300

    Update MELT_MT24S2A.h

--- src/MELT_MT24S2A.h
+++ src/MELT_MT24S2A.h
@@ -1,7 +1,12 @@
 /**
  * @file MELT_MT24S2A.h
  * @brief Универсальная библиотека для LCD дисплея MT24S2A
- * @version 3.0.0 (упрощённый конструктор, HAL в корне библиотеки)
+ * @author Reinhardt Michael
+ * @version 2.0.1
+ * @date 2024
+ * 
+ * Поддерживаемые платформы: Arduino, AVR, STM32, ESP8266, ESP32
+ * Добавлена поддержка пина RW, автовыбор HAL, getMillis()
  */
 
 #ifndef MELT_MT24S2A_H
@@ -10,29 +15,29 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// ---- Автовыбор HAL по платформе (файлы лежат в корне библиотеки) ----
+// ---- Автовыбор HAL ----
 #if defined(ARDUINO)
-    #include "LCD_HAL_Arduino.h"
+    #include "src/hal/LCD_HAL_Arduino.h"
     typedef LCD_HAL_Arduino LCD_HAL_Platform;
 #elif defined(__AVR__) && !defined(ARDUINO)
-    #include "LCD_HAL_AVR.h"
+    #include "src/hal/LCD_HAL_AVR.h"
     typedef LCD_HAL_AVR LCD_HAL_Platform;
 #elif defined(STM32) || defined(PLATFORM_STM32)
-    #include "LCD_HAL_STM32.h"
+    #include "src/hal/LCD_HAL_STM32.h"
     typedef LCD_HAL_STM32 LCD_HAL_Platform;
 #elif defined(ESP8266)
-    #include "LCD_HAL_ESP.h"
+    #include "src/hal/LCD_HAL_ESP.h"
     typedef LCD_HAL_ESP LCD_HAL_Platform;
 #elif defined(ESP32)
-    #include "LCD_HAL_ESP.h"
+    #include "src/hal/LCD_HAL_ESP.h"
     typedef LCD_HAL_ESP LCD_HAL_Platform;
 #else
     #error "Неподдерживаемая платформа!"
 #endif
 
-#include "LCD_HAL.h"   // базовый интерфейс HAL
+#include "src/hal/LCD_HAL.h"
 
-// ---- Команды LCD (HD44780) ----
+// ---- Команды LCD ----
 #define LCD_CLEAR_DISPLAY       0x01
 #define LCD_RETURN_HOME         0x02
 #define LCD_ENTRY_MODE_SET      0x04
@@ -68,21 +73,24 @@
 
 class MELT_MT24S2A {
 public:
-    // Конструкторы с внешним HAL
-    MELT_MT24S2A(LCD_HAL* hal, uint8_t rs, uint8_t enable,
+    // Конструкторы (все содержат rw)
+    MELT_MT24S2A(LCD_HAL* hal, uint8_t rs, uint8_t rw, uint8_t enable,
                  uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
                  uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
-    MELT_MT24S2A(LCD_HAL* hal, uint8_t rs, uint8_t enable,
+    MELT_MT24S2A(LCD_HAL* hal, uint8_t rs, uint8_t rw, uint8_t enable,
                  uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
     MELT_MT24S2A(LCD_HAL* hal, uint8_t i2c_address);
 
-    // Упрощённый конструктор (создаёт внутренний HAL автоматически)
-    MELT_MT24S2A(uint8_t rs, uint8_t enable,
+    // Простой конструктор (без HAL) – автоматически создаёт внутренний HAL
+    MELT_MT24S2A(uint8_t rs, uint8_t rw, uint8_t enable,
                  uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                 uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+    MELT_MT24S2A(uint8_t rs, uint8_t rw, uint8_t enable,
                  uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
 
     ~MELT_MT24S2A();
 
+    // Базовые методы
     void begin(uint8_t cols, uint8_t rows);
     void clear();
     void home();
@@ -91,26 +99,35 @@ public:
     void print(const char* text);
     void print(int number);
 
-    void noDisplay(); void display();
-    void noCursor(); void cursor();
-    void noBlink(); void blink();
-    void scrollDisplayLeft(); void scrollDisplayRight();
-    void leftToRight(); void rightToLeft();
-    void autoscroll(); void noAutoscroll();
+    // Управление дисплеем
+    void noDisplay();
+    void display();
+    void noCursor();
+    void cursor();
+    void noBlink();
+    void blink();
+    void scrollDisplayLeft();
+    void scrollDisplayRight();
+    void leftToRight();
+    void rightToLeft();
+    void autoscroll();
+    void noAutoscroll();
     void createChar(uint8_t location, uint8_t charmap[8]);
 
+    // Высокоуровневые методы
     void printRus(const char* text);
     void printInt(int32_t num);
     void printIntBlink(int32_t num, int cursorPos, bool show);
     void printVdBlink(int32_t vd_milli, int cursorPos, bool show);
     void clearField(int width);
-    unsigned long getMillis() { return millis(); }
+
+    unsigned long getMillis();
 
     const char* getPlatformName();
 
 private:
     LCD_HAL* _hal;
-    uint8_t _rs_pin, _enable_pin;
+    uint8_t _rs_pin, _rw_pin, _enable_pin;
     uint8_t _data_pins[8];
     uint8_t _cols, _rows;
     uint8_t _displayfunction, _displaycontrol, _displaymode;
@@ -124,4 +141,4 @@ private:
     uint8_t utf8_to_hd44780(const char*& text);
 };
 
-#endif // MELT_MT24S2A_H
+#endif
